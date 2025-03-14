084643aca4648b5969276168e7ba36cbe583066f
cosmic1616 <cosmiccrazy16@gmail.com>
2025-03-14 21:01:17 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,18 +8,18 @@ import os
 
 
 class Config(object):
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
+	BOT_TOKEN = os.environ.get("1234492420:AAEbzGhVani42brYK6KKT9WPb3lpuJae7h0")
 	API_ID = int(os.environ.get("API_ID", "23630387"))
 	API_HASH = os.environ.get("API_HASH" , "3139998ebc89c2bc1873b794689f8e56")
 	STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "NoNeed")
 	STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", "NoNeed")
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL","-1002396213226"))
+	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL","-1002690244679"))
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
 	DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
 	PRESET = os.environ.get("PRESET", "ultrafast")
-	OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
+	OWNER_ID = int(os.environ.get("OWNER_ID", 958696007))
 	CAPTION = "By @hacking_freak"
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "WATERMARK_SW_BOT")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "spark_bova_bot")
 	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://maan:3pxZzpu0oXBfki7l@cluster0.6i9il.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	ALLOW_UPLOAD_TO_STREAMTAPE = bool(os.environ.get("ALLOW_UPLOAD_TO_STREAMTAPE", False))
